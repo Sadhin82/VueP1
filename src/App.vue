f24fabb569fb977c8a1734fe8cbe12485c477d2b
Sadhin <sadhinsharkar564@gmail.com>
2024-11-29 11:34:04 +0600

    vue p1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HeroSection from './components/HeroSection.vue';
+import HeaderSection from './components/HeaderSection.vue';
+import FeaturedProduct from './components/FeaturedProduct.vue';
+import ProductCollections from './components/ProductCollections.vue';
+import StanderdProducts from './components/StanderdProducts.vue';
+import ContactSection from './components/ContactSection.vue';
+import FooterSection from './components/FooterSection.vue';
+</script>
+
+<template>
+  <header-section/>
+  <hero-section/>
+  <standerd-products/>
+  <product-collections/>
+ <featured-product/>
+<contact-section/>
+<footer-section/>
+
+  
+
+
+
+</template>
+
+<style scoped></style>
---
+++ src/components/ContactSection.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ContactInfo from './contact/ContactInfo.vue';
+import ContactForm from './contact/ContactForm.vue';
+</script>
+
+<template>
+<section class="p-4 py-20">
+    <div class="mx-auto max-w-7xl w-full">
+      <div class="grid lg:grid-cols-2 grid-cols-1 items-center">
+        <contact-info phone="111-223-55" email="me@sadhin.me" address="Nakla,Sherpur" />
+        <contact-form :displayPhone = false />
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+</style>
